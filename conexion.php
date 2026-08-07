3657fb6dd68863a5ff0f83e2559ca08e1fc092db
jkarreno <jkarreno@gmail.com>
2026-08-07 14:17:03 -0600

    ADD variables env

--- conexion.php
+++ conexion.php
@@ -1,14 +1,14 @@
 <?php
-//require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-//use Dotenv\Dotenv;
+use Dotenv\Dotenv;
 
 // Cargar el archivo .env
-//$dotenv = Dotenv::createImmutable(__DIR__.'/..');
-//$dotenv->load();
+$dotenv = Dotenv::createImmutable(__DIR__.'/');
+$dotenv->load();
 
 
-$conn=mysqli_connect('localhost', 'user_similares', 'GptSimi26#', 'base_similares');
+$conn=mysqli_connect($_ENV['HOST_DATA_BASE'], $_ENV['USER_DATA_BASE'], $_ENV['PASS_DATA_BASE'], $_ENV['DATA_BASE']);
 
 
 
